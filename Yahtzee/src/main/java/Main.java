1ff08aa3f052e7e253b5b353b97e8b09559e6362
Jim Doyle <indellibleink@gmail.com>
2020-09-17 22:42:39 -0400

    Put 5 Dies into a List

--- Yahtzee/src/main/java/Main.java
+++ Yahtzee/src/main/java/Main.java
@@ -6,23 +6,19 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Roll Die 10x");
-
-
         //  Rolling 1 Die 10 times.
+        //  System.out.println("Roll Die 10x");
         //  for (int idx = 0; idx < 10; idx++)
-       //     System.out.println(die.roll());
+        //     System.out.println(die.roll());
 
         // Making a List of Dice that have been rolled.
         List<Die> dice = new ArrayList<Die>();
-
+        // put 5 Dies into a List
         for (int idx = 0; idx < 5; idx++) {
             Die die = new Die(6);
             dice.add(die);
         }
-        for (Die cube : dice)
-            System.out.println(cube.getRoll());
-
     }
+
 }
 
